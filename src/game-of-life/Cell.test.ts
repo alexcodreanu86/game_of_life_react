3dc9df71f37f7e00a74e14d8ce08609473369c81
Alexandru Codreanu <acodreanu@rewardsnetwork.com>
2018-11-17 07:32:49 -0600

    Add cell knowledge of neighbors.

--- src/game-of-life/Cell.test.ts
+++ src/game-of-life/Cell.test.ts
@@ -7,5 +7,26 @@ describe('Cell', () => {
 
       expect(cell.isNeighbor(cell)).toBe(false);
     });
+
+    it('returns false when the neighbor is far away on x axis', () => {
+      const cell = new Cell(0, 0);
+      const distantCell = new Cell(2, 0);
+
+      expect(cell.isNeighbor(distantCell)).toBe(false);
+    });
+
+    it('returns false when the neighbor is far away on y axis', () => {
+      const cell = new Cell(0, 0);
+      const distantCell = new Cell(0, 2);
+
+      expect(cell.isNeighbor(distantCell)).toBe(false);
+    });
+
+    it('returns true when other cell is a neighbor', () => {
+      const cell = new Cell(0, 0);
+      const distantCell = new Cell(1, 1);
+
+      expect(cell.isNeighbor(distantCell)).toBe(true);
+    });
   });
 });
--- src/game-of-life/Cell.ts
+++ src/game-of-life/Cell.ts
@@ -1,9 +1,22 @@
+const NEIGHBOR_DISTANCE = 1;
+
 class Cell {
   constructor(public x: number, public y: number) {}
 
   public isNeighbor(other: Cell): boolean {
-    return !(this.x === other.x && this.y === other.y)
+    const {x, y} = this;
+    const neighborOnX = Math.abs(x - other.x) <= NEIGHBOR_DISTANCE;
+    const neighborOnY = Math.abs(y - other.y) <= NEIGHBOR_DISTANCE;
+
+    return neighborOnX &&
+      neighborOnY &&
+      !this.equals(other);
+  }
+
+  private equals(other: Cell) {
+    return this.x === other.x &&
+      this.y === other.y;
   }
 }
 
-export default Cell
+export default Cell;
