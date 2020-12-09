47846c94d23f6713c85fde82a5b64b808e0fac5a
Alex Codreanu <alexcodreanu86@gmail.com>
2020-12-08 22:32:43 -0600

    Use cell behavior in reducer tests.

--- src/GameOfLife/Components/game-of-life-reducer.test.ts
+++ src/GameOfLife/Components/game-of-life-reducer.test.ts
@@ -1,3 +1,4 @@
+import { CellBehavior } from '../Cell';
 import gameOfLifeReducer, {
   addCellAction,
   calculateNextGenerationAction,
@@ -8,25 +9,29 @@ import gameOfLifeReducer, {
 describe('gameOfLifeReducer', () => {
   it('defaultState has empty world', () => {
     expect(defaultState.world.size).toEqual(4)
-    expect(defaultState.world.cells).toEqual([{ x: 0, y: 0 }])
+    expect(defaultState.world.cells).toEqual([CellBehavior.new(0, 0)])
   })
 
   it('adds a cell', () => {
-    const { world: { cells }} = gameOfLifeReducer(defaultState, addCellAction({ x: 1, y: 1 }))
-    expect(cells).toEqual([{x: 0, y: 0}, {x: 1, y: 1}])
+    const { world: { cells }} = gameOfLifeReducer(defaultState, addCellAction(CellBehavior.new(1, 1)))
+    expect(cells).toEqual([CellBehavior.new(0, 0), CellBehavior.new(1, 1)])
   })
 
   it('removes a cell', () => {
-    const { world: { cells }} = gameOfLifeReducer(defaultState, removeCellAction({ x: 0, y: 0 }))
+    const { world: { cells }} = gameOfLifeReducer(defaultState, removeCellAction(CellBehavior.new(0, 0)))
     expect(cells).toEqual([])
   })
 
   it('calculates next generation', () => {
-    const stateWithTwoCells = gameOfLifeReducer(defaultState, addCellAction({ x: 0, y: 1 }))
-    const stateWithThreeCells = gameOfLifeReducer(stateWithTwoCells, addCellAction({ x: 0, y: 2 }))
+    const stateWithTwoCells = gameOfLifeReducer(defaultState, addCellAction(CellBehavior.new(0, 1)))
+    const stateWithThreeCells = gameOfLifeReducer(stateWithTwoCells, addCellAction(CellBehavior.new(0, 2)))
 
     const { world: { cells }} = gameOfLifeReducer(stateWithThreeCells, calculateNextGenerationAction())
 
-    expect(cells).toEqual([{x: 0, y: 1}, {x: -1, y: 1}, {x: 1, y: 1}])
+    expect(cells).toEqual([
+      CellBehavior.new(0, 1),
+      CellBehavior.new(-1, 1),
+      CellBehavior.new(1, 1)
+    ])
   })
 })
